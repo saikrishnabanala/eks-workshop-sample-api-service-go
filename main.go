389d62811726f47143bf1d12a98edbed9690c26b
saikrishnabanala <43780789+saikrishnabanala@users.noreply.github.com>
2019-04-16 18:58:34 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "DATA FOUNDRY"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
